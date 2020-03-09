41a4d1a4bd99e7a2386b320ce2149dad9bdd3403
CISCO <cisco@iicsadmin.com>
2020-03-09 13:08:20 +0800

    JSON

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-setTimeout(() => {
- document.write("Customer 1 was entertained...");
-}, 2000);
-document.write("Customer 2 was entertained..");
+let person = {
+    name: "Juan", age: 18, course:"IT", address:{
+        number:156, street:"Batangas St.", city:"Quezon City"
+    }
+}
+document.write(person["name"]);
